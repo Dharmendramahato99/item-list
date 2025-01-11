ed19d3fc1f26f217498557cabf73fa64ac7fc320
Dharmendra <mahatodharmendra726@gmail.com>
2025-01-11 13:53:36 +0530

    Second Commit

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>
-      <div className='font-bold bg-gray-600 text-white text-center p-4 m-4'>Item List Manager</div>
+    <div>
       
+      <h1 className='font-bold bg-gray-600 text-white text-center p-4 m-4'>Item List Manager</h1>
     </div>
   )
 }
